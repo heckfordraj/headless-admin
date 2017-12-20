59b5b0d2b4ff2b182a8963cb66ae680696b6c387
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2017-12-20 12:00:54 +0000

    added RoutingModule, added PagesComponent, added EditorComponent, moved AppComponent logic to PagesComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import { RoutingModule } from './routing.module';
 import { ServerService } from './server.service';
 
+import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { EditorComponent } from './editor/editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    EditorComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RoutingModule
   ],
   providers: [ServerService],
   bootstrap: [AppComponent]
---
+++ src/app/routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes }  from '@angular/router';
+
+import { PagesComponent } from './pages/pages.component';
+import { EditorComponent } from './editor/editor.component';
+
+
+const routes: Routes = [
+  { path: 'pages', component: PagesComponent },
+  { path: 'page/:id', component: EditorComponent },
+  { path: '', redirectTo: '/pages', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class RoutingModule { }
